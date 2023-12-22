3dc4663ed76650a93f0e023c1fafecb2ada0e7ff
Hrishi Punde <hrishipunde9882@gmail.com>
2023-12-22 17:19:46 +0530

    Overriden classes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,34 @@
 package com.driver;
 
+
+   class A {
+       public String meth() {
+           return "Invoking method from class A";
+
+       }
+   }
+       class B extends A{
+
+           @Override
+           public String meth() {
+               return "Method is overridden in Extended class B";
+
+           }
+   }
+
 public class Main {
-  
+       public static void main(String[] args){
+           B objB= new B();
+
+           String resultFromA = objB.meth();
+           System.out.println(resultFromA);
+
+           String resultFromB = objB.meth();
+           System.out.println(resultFromB);
+
+
+
+       }
+
+
 }
